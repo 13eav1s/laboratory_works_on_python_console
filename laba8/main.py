31ff9f9cdfe0d8f8cb151a4b3f4ba03c4a8e9b52
13eav1s <krasnovleonid2003@gmail.com>
2021-10-31 20:21:30 +0300

    Лаба 8 Уже работают несколько пунктов

--- laba8/main.py
+++ laba8/main.py
@@ -20,14 +20,79 @@ def PrintMenu():
     print("Меню: \n"
           "1. Ввести матрицу \n"
           "2. Добавить строку \n"
-          "3. Удалить произвольный элемент из списка (по номеру) \n"
-          "4. Очистить список \n"
-          "5. Поиск элемента с наибольшим числом английских заглавных"
-          " букв \n"
-          "6. Замена всех английских заглавных букв на строчные \n"
-          "0. Выйти из программы")
+          "3. Удалить строку \n"
+          "4. Добавить столбец \n"
+          "5. Удалить столбец \n"
+          "6. Найти строку, имеющую определённое свойство по варианту \n"
+          "7. Переставить местами строки с наибольшим и наименьшим количеством отрицательных элементов \n"
+          "8. Найти столбец, имеющий определённое свойство по варианту \n"
+          "9. Переставить местами столбцы с максимальной и минимальной суммойэлеметов \n"
+          "10. Вывести текущую матрицу \n"
+          "0. Выйти из программы \n")
     while True:
         elem = int(input("Введите номер пункта: "))
         if 0 <= elem <= 7:
             break
     return elem
+
+
+def ClearArray(matrix):
+    matrix.clear()
+    print("Матрица очищена")
+    return matrix
+
+
+def OutMatrix(a):
+    print("Матрица выглядит так: ")
+    for i in range(len(a)):
+        for j in range(len(a[i])):
+            print(a[i][j], end=" ")
+        print()
+    print()
+    return 0
+
+
+def ClearAndFill(matrix):
+    ClearArray(matrix)
+    matrix_column = int(input("Введите колличество столбцов: "))
+    matrix_string = int(input("Введите колличество строчек: "))
+    for i in range(matrix_string):
+        matrix.append([0]*matrix_column)
+    for i in range(matrix_string):
+        for j in range(matrix_column):
+            matrix[i][j] = int(input("Введите элемент, который хотите добавить: "))
+    OutMatrix(matrix)
+    return matrix
+
+
+def AddString(matrix):
+    matrix.append([0]*len(matrix[0]))
+    for i in range(len(matrix[0])):
+        matrix[len(matrix) - 1][i] = int(input("Введите элемент который хотите добавить: "))
+    OutMatrix(matrix)
+
+
+def DelString(matrix):
+    index = int(input("Введите номер строки, которую хотите удалить: "))
+    matrix.pop(index)
+    OutMatrix(matrix)
+
+
+mass = []
+item = PrintMenu()
+while True:
+    if item == 0:
+        print("Программа завершила работу")
+        break
+    if item == 1:
+        ClearAndFill(mass)
+        item = PrintMenu()
+    if item == 2:
+        AddString(mass)
+        item = PrintMenu()
+    if item == 3:
+        DelString(mass)
+        item = PrintMenu()
+    if item == 10:
+        OutMatrix(mass)
+        item = PrintMenu()
