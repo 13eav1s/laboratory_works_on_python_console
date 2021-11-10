0384e7eb7aae88f6ad63bbb45ddc2571a03880f9
13eav1s <45150259+13eav1s@users.noreply.github.com>
2021-11-10 11:07:58 +0300

    laba9 5th file

--- laba9/1st_file/main.py
+++ laba9/1st_file/main.py
@@ -1,12 +1,51 @@
 """
-Даны массивы D и F. Сформировать матрицу A по формуле
-Ajk = sin(Dj+Fk).
-Определить среднее арифметическое положительных чисел каждой строки
-матрицы и количество элементов, меньших среднего арифметического.
-Результаты записать соответственно в массивы AV и L. Напечатать матрицу A в
-виде матрицы и рядом столбцы AV и L.
+Лабораторная работа №9 1-й файл
+ИУ7-11Б
+Краснов Леонид
 """
 
+import math as m
 
 
-length = int(input("Введите длину строки: "))
+D = list(map(float, input(f"Введите строку: ").split()))
+F = list(map(float, input(f"Введите строку: ").split()))
+a = []
+for i in range(len(D)):
+    a.append([0]*len(F))
+
+for j in range(len(D)):
+    for k in range(len(F)):
+        a[j][k] = m.sin(D[j] + F[k])
+# print("Матрица выглядит так: ")
+# for i in range(len(a)):
+#     for j in range(len(a[i])):
+#         print(a[i][j], end=" ")
+#     print()
+average = 0
+AV = []
+L = []
+
+for i in range(len(a)):
+    plusElem = 0
+    for j in range(len(a[i])):
+        if a[i][j] >= 0:
+            average += a[i][j]
+            plusElem += 1
+    if plusElem == 0:
+        average = 0
+    else:
+        average /= plusElem
+    # print("Среднеарифметическое", i, "строки = ", average)
+    AV.append(average)
+    lowElem = 0
+    for j in range(len(a[i])):
+        if a[i][j] < average:
+            lowElem += 1
+    L.append(lowElem)
+    average = 0
+
+for i in range(len(a)):
+    for j in range(len(a[i])):
+        print(a[i][j], end=" ")
+    print("     ", AV[i], L[i], end=" ")
+    print()
