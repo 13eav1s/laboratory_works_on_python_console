25f1e31641b95d3269f1f864b0994b72b05703f9
13eav1s <45150259+13eav1s@users.noreply.github.com>
2021-11-29 14:49:04 +0300

    123

---
+++ laba11/defend/main.py
@@ -0,0 +1,41 @@
+"""
+Функция кос икс
+Метод трапеций
+Заданная точность
+"""
+import math as m
+
+
+def F(x):
+    y = m.cos(x)
+    return y
+
+
+def Trapezoid(A, B, N):
+    e = 1e-7
+    h = (B - A) / N
+    x = A
+    rez = 0
+    while e < abs(x - (B - h)):
+        rez += ((F(x) + F(x + h)) / 2) * ((x + h) - x)
+        x += h
+    return rez
+
+
+start = float(input("Введите начало: "))
+stop = float(input("Введите конец: "))
+#  N = int(input("Введите N1: "))
+E = float(input("Введте желаемую погрешность: "))
+N = 1
+x1 = Trapezoid(start, stop, N)
+x2 = Trapezoid(start, stop, N * 2)
+TrapE = abs(x1 - x2)
+while E <= TrapE:
+    N *= 2
+    x1 = x2
+    x2 = Trapezoid(start, stop, N * 2)
+    TrapE = abs(x1 - x2)
+
+print("Результат: ", x1)
+print("N = ", N)
+print("Погрешность: ", TrapE)
