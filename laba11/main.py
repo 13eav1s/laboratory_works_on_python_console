925674035ce854163d866035c3a09282102e14c8
13eav1s <45150259+13eav1s@users.noreply.github.com>
2021-12-12 23:20:01 +0300

    конец

--- laba11/main.py
+++ laba11/main.py
@@ -29,7 +29,8 @@ def PrintMenu():
           "5. Замена одного слова другим во всём тексте \n"
           "6. Вычисление арифметических выражений внутри текста операции сложения и вычитания \n"
           "7. Найти предложение, в котором количество слов, содержащих каждую букву 2 и более раз, максимально. \n"
-          "0. Выйти из программы \n")
+          "8. Отключить выравнивание. \n"
+          "0. Выйти из программы. \n")
     while True:
         elem = int(input("Введите номер пункта: "))
         if 0 <= elem <= 10:
@@ -38,63 +39,66 @@ def PrintMenu():
 
 
 #  Функция выравнивает текст по левому краю
-def left(t):
-    for i in range(len(t)):
+def left(txt):
+    for i in range(len(txt)):
         #  print(t[i])
-        t[i] = t[i].strip()
-    return t
+        txt[i] = txt[i].strip()
+    return txt
 
 
-def right(t):
-    for i in range(len(t)):
+#  Функция для выравнивания текста по правой стороне
+def right(txt):
+    for i in range(len(txt)):
         #  print(t[i])
-        t[i] = t[i].strip()
+        txt[i] = txt[i].strip()
 
     maxim = 0
-    for i in range(len(t)):
-        if len(t[i]) > maxim:
-            maxim = len(t[i])
-    for i in range(len(t)):
-        t[i] = t[i][::-1]
-        t[i] += " " * (maxim - len(t[i]))
-        t[i] = t[i][::-1]
-    return t
+    for i in range(len(txt)):
+        if len(txt[i]) > maxim:
+            maxim = len(txt[i])
+    for i in range(len(txt)):
+        txt[i] = txt[i][::-1]
+        txt[i] += " " * (maxim - len(txt[i]))
+        txt[i] = txt[i][::-1]
+    return txt
 
 
 #  Функция для вывода текста
-def textOut(t):
-    for i in range(len(t)):
-        for j in range(len(t[i])):
-            print(t[i][j], end='')
+def textOut(txt):
+    for i in range(len(txt)):
+        for j in range(len(txt[i])):
+            print(txt[i][j], end='')
         print()
 
 
-def width(t):
-    maxi = 0
-    for i in range(len(t)):
-        t[i] = t[i].strip()
-        if len(t[i]) > maxi:
-            maxi = len(t[i])
+# def width(t):
+#     maxi = 0
+#     for i in range(len(t)):
+#         t[i] = t[i].strip()
+#         if len(t[i]) > maxi:
+#             maxi = len(t[i])
+#
+#     for i in range(len(t)):
+#         t[i] = t[i].center(maxi)
+#
+#     return t
 
-    for i in range(len(t)):
-        t[i] = t[i].center(maxi)
 
-    return t
-
-
-def delWord(t):
+#  Функция для удаления слова
+def delWord(txt):
     word = input("Введите слово, которое хотите удолить: ")
-    for i in range(len(t)):
-        t[i] = t[i].replace(word, '')
-    return t
+    for i in range(len(txt)):
+        txt[i] = txt[i].replace(word, '')
+    return txt
 
 
-def renameWord(t):
+#  Функция для замены слова
+def renameWord(txt):
     word = input("Введите слово, которое хотите заменить на другое: ")
     word2 = input("Введите слово, на которое вы хотите заменить")
-    for i in range(len(t)):
-        t[i] = t[i].replace(word, word2)
-    return t
+    for i in range(len(txt)):
+        txt[i] = txt[i].replace(word, word2)
+    return txt
 
 
 # def math(t):
@@ -127,6 +131,60 @@ def renameWord(t):
 #     return t
 
 
+#  Функция для нахождения предложения с налибольшим числом слов, в которых встречается любая буква больше 2-х раз
+def findSentence(txt):
+    alphabet = ["А", "а", "Б", "б", "В", "в", "Г", "г", "Д", "д", "Е", "е", "Ё", "ё", "Ж", "ж", "З", "з", "И", "и",
+                "Й", "й", "К", "к", "Л", "л", "М", "м", "Н", "н", "О", "о", "П", "п", "Р", "р", "С", "с", "Т", "т",
+                "У", "у", "Ф", "ф", "Х", "х", "Ц", "ц", "Ч", "ч", "Щ", "щ", "Ъ", "ъ", "Э", "э", "Ь", "ь", "Э", "э",
+                "Ю", "ю", "Я", "я", "Ы", "ы"]
+    sentence = []
+    sentences = []
+    word = []
+    for i in range(len(txt)):
+        for j in range(len(txt[i])):
+            if txt[i][j] == '.':
+                sentences.append(sentence.copy())
+                #  print(sentence)
+                sentence.clear()
+            else:
+                sentence.append(txt[i][j])
+    #  print(sentences)
+    #  del sentence
+    sentence = []
+    savei = 0
+    maxCommon = 0
+    colCommon = []
+    for i in range(len(sentences)):
+        for j in range(len(sentences[i])):
+            if sentences[i][j] == " " or sentences[i][j] == "." or sentences[i][j] == ",":
+                sentence.append(word.copy())
+                word.clear()
+            else:
+                word.append(sentences[i][j])
+
+        colWords = 0
+        for j in range(len(sentence)):
+            for e in range(len(alphabet)):
+                colCommon.append(sentence[j].count(alphabet[e]))
+                if max(colCommon) >= 2:
+                    #  print(i, colCommon, sentence[j])
+                    colWords += 1
+                    colCommon = []
+                    break
+                colCommon = []
+        if colWords > maxCommon:
+            maxCommon = colWords
+            savei = i
+        sentence.clear()
+    print("Максимальное колличество слов с повторяющимися буквами в предложении")
+    for i in range(len(sentences[savei])):
+        print(sentences[savei][i], end="")
+        if i % 70 == 0:
+            print()
+    print(".\n")
+
+
+#  Функция выполняет арифметические операции в тексте
 def arif(a):
     b = []
     for i in range(len(a)):
@@ -140,7 +198,7 @@ def arif(a):
                 elif len(c) > 0:
                     b.append(int(c))
                     c = ''
-                if j == len(line[k])-1 and len(c) > 0:
+                if j == len(line[k]) - 1 and len(c) > 0:
                     b.append(int(c))
                     c = ''
                 if line[k][j] == '+' or line[k][j] == '-':
@@ -152,23 +210,23 @@ def arif(a):
                 else:
                     count = -b[1]
                     start = 2
-                for j in range(start,len(b)-1,2):
+                for j in range(start, len(b) - 1, 2):
                     if b[j] == '-':
-                        count -= b[j+1]
+                        count -= b[j + 1]
                     elif b[j] == '+':
-                        count += b[j+1]
+                        count += b[j + 1]
                 print(count)
                 line[k] = count
             b.clear()
         a[i] = ''
         for j in range(len(line)):
-            a[i] += str(line[j])+' '
+            a[i] += str(line[j]) + ' '
     return a
 
 
 # Исходный текст
-"""
-text = ["С конца 1811-го года 5+45 началось усиленное вооружение и сосредоточение сил Западной Европы,",
+
+text = ["Вот как же так. С конца 1811-го года 5+45 началось усиленное вооружение. и сосредоточение сил Западной Европы,",
         " и в 1812 году силы эти — миллионы людей (считая тех, которые перевозили и кормили армию) ",
         "двинулись с Запада на Восток, к границам России, к которым точно так же с 1811-го года стягивались силы",
         " России. 12 июня силы Западной Европы перешли границы России, и началась война, то есть",
@@ -176,13 +234,41 @@ text = ["С конца 1811-го года 5+45 началось усиленно
         " друг, против друга такое бесчисленное количество злодеяний, обманов, измен,",
         " воровства, подделок и выпуска фальшивых ассигнаций, грабежей, поджогов",
         " и убийств, которого в целые века не соберет летопись всех судов мира и на которые,",
-        " в этот период времени, люди, совершавшие их, не смотрели как на преступления."]
-"""
-text = ["23+23,",
-        "asd fsa fsda + fads,",
-        " dsf df 4-5 dfa dsf dsf,",
-        "4 dd - 44 -as fd 10,",
-        "10-15"]
+        " в этот период времени, люди, совершавшие их, не смотрели как на преступления. как. кук."]
+t = text.copy()
+
+
+# Функция выравнивания списка строк по ширине
+def width_alig(li):
+    li[0] = li[0].strip()
+    max_row_len = len(li[0])
+    for i in range(1, len(li)):
+        li[i] = li[i].strip()
+        if len(li[i]) > max_row_len:
+            max_row_len = len(li[i])
+    for i in range(len(li)):
+        while len(li[i]) != max_row_len:
+            for j in range(len(li[i])):
+                if len(li[i]) != max_row_len:
+                    if li[i][j] == " ":
+                        li[i] = li[i][:j + 1] + "$" + li[i][j + 1:]
+        li[i] = li[i].replace("$", " ")
+    return li
+
+
+# Функция для отключения выравнивания (Уже не используется)
+def off_width(txt):
+    for i in range(len(txt) - 1):
+        if txt[i] == " " and txt[i + 1] == " ":
+            txt[i + 1] = ""
+    return txt
+
+
+# text = ["23+23,",
+#         "asd fsa + fads,",
+#         " dsf df 4-5 dfa dsf dsf,",
+#         "4 dd - 44 -as fd 10,",
+#         "10-15"]
 
 # for i in range(len(text)):
 #     text[i] = str(text[i][0])
@@ -209,6 +295,10 @@ text = ["23+23,",
 #         text[i][j].split()
 # print(text)
 # textOut(text)
+''''''
+
+#  точка входа в программу
+
 item = PrintMenu()
 while True:
     if item == 0:
@@ -221,7 +311,7 @@ while True:
         textOut(right(text))
         item = PrintMenu()
     if item == 3:
-        textOut(width(text))
+        textOut(width_alig(text))
         item = PrintMenu()
     if item == 4:
         textOut(delWord(text))
@@ -231,5 +321,12 @@ while True:
         item = PrintMenu()
     if item == 6:
         textOut(arif(text))
+        item = PrintMenu()
+    if item == 7:
+        findSentence(text)
+        item = PrintMenu()
+    if item == 8:
+        text = t.copy()
+        print("Отключены все выравнивания")
         item = PrintMenu()
 print(text)
